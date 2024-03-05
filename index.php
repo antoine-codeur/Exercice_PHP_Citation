c7c6c40e8159f485c863fc417b6adc89767acd55
antoine-codeur <antoinejf.denoue@gmail.com>
2024-03-05 13:50:18 +0100

    ajout des auteurs

--- index.php
+++ index.php
@@ -47,17 +47,19 @@ $nombreCitations = $_SESSION['nombreCitations'];
                 <input type="number" id="nombreCitations" name="nombreCitations" min="1" value="<?php echo $nombreCitations; ?>">
             </div>
             <input type="hidden" name="generate" value="1">
-            <button type="submit" name="Citation">Générer des Citations</button>
+            <button id="Gen_Button" type="submit" name="Citation">Générer des Citations</button>
         </form>
 
         <?php if (!empty($citations)): ?>
-        <div id="citation_result">
-            <?php foreach ($citations as $citation): ?>
-                <p><?php echo htmlspecialchars($citation); ?></p>
-                <a href="src/download.php?citation=<?php echo urlencode($citation); ?>" class="download-link">Télécharger en PNG</a>
-            <?php endforeach; ?>
-        </div>
+            <div id="citation_result">
+                <?php foreach ($citations as $item): ?>
+                    <p><?php echo htmlspecialchars($item['citation']); ?></p>
+                    <p><em><?php echo htmlspecialchars($item['auteur']); ?></em></p>
+                    <a href="src/download.php?citation=<?php echo urlencode($item['citation']); ?>" class="download-link">Télécharger en PNG</a>
+                <?php endforeach; ?>
+            </div>
         <?php endif; ?>
+
     </div>
     <script src="asset/script/themeDark.js"></script>
 </body>
--- src/CitationGenerator.php
+++ src/CitationGenerator.php
@@ -3,22 +3,29 @@ class CitationGenerator {
     private $debut;
     private $milieu;
     private $fin;
+    private $auteurs;
 
     public function __construct() {
-        $jsonPath = __DIR__ . '/fragments.json';
+        $jsonPath = __DIR__ . '/json/fragments.json';
         $jsonContent = file_get_contents($jsonPath);
         $fragments = json_decode($jsonContent, true);
 
-    
         $this->debut = $fragments['debut'];
         $this->milieu = $fragments['mid'];
         $this->fin = $fragments['fin'];
+        $this->auteurs = $fragments['auteurs'];
     }
 
     public function genererCitation() {
         $partie1 = $this->debut[array_rand($this->debut)];
         $partie2 = $this->milieu[array_rand($this->milieu)];
         $partie3 = $this->fin[array_rand($this->fin)];
-        return $partie1 . ' ' . $partie2 . ' ' . $partie3;
+        $auteur = $this->auteurs[array_rand($this->auteurs)];
+
+    
+        return [
+            'citation' => '"' . $partie1 . ' ' . $partie2 . ' ' . $partie3 . '"',
+            'auteur' => $auteur
+        ];
     }
 }
